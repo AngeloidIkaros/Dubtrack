a0f25e1b60ef8c91c30d1307b4af20542a177601
AngeloidIkaros <thomas.henne97@gmail.com>
2017-06-06 04:29:25 +0200

    Update stream_status.user.js

--- Dubtrack Stream Tab/stream_status.user.js
+++ Dubtrack Stream Tab/stream_status.user.js
@@ -10,21 +10,35 @@
 
 (function(){
 
-    setTimeout(function() {
-        var $streamtab = document.createElement("span");
-        $streamtab.className = "streamers";
-        $streamtab.append("Streamers");
-        $($streamtab).insertAfter( ".room-info-display" );
+    setInterval(function() {
+        user_name = "sodapoppin";
+        api_key = "5j0r5b7qb7kro03fvka3o8kbq262wwm";
+        twitch_widget = $("#twitch-widget");
 
-        var $stream_display = document.createElement("div");
-        $stream_display.id = 'streamer-display';
-        $($stream_display).insertAfter( " .player_header" );
-        $stream_display.style.display = 'none';
+        twitch_widget.attr("href","https://twitch.tv/" + user_name);
+
+        $.getJSON('https://api.twitch.tv/kraken/streams/' + user_name + '?client_id=' + api_key + '&callback=?', function(data) {	
+            if (data.stream) {
+                twitch_widget.html("<span class='online'></span>"+ user_name + " Online! Playing: " +  data.stream.game + "<span class='viewers'>Viewers: " + data.stream.viewers + "</span>");
+            } else {
+                twitch_widget.html("<span class='offline'></span> Offline");
+            }  
+        });
+    }, 60000);
+
+
+    setTimeout(function() {
+        var streamerstab = $('<span class="streamers">Streamers</span>');
+        streamerstab.insertAfter("#main_player .room-info-display");
+
+
+        var streamers_display = $('<div id="streamer-display" style="display: none;"><div class="stream-display-container"><h2 class="stream-header">IKAS STREAMER LIST</h2></div><div class="description"><a href="#" class="twitch-widget" id="twitch-widget" target="_blank"></a></div>');
+        streamers_display.insertAfter(".player_header");
 
         /* Hides all other tabs except the streamer one and add/removes active in the class name */
         $('#main_player .streamers')[0].onclick = function() {
-            $stream_display.style.display = 'block';
-            $streamtab.className = "streamers active";
+            streamers_display[0].style.display = "block";
+            streamerstab.addClass("active");
             $('#main_player #room-info-display')[0].style.display = 'none';
             $('#main_player .room-info-display')[0].className = 'room-info-display';
             $('#main_player #mods-controllers')[0].style.display = 'none';
@@ -35,19 +49,15 @@
         /* Hides the streamer tab if u click video or Info */
 
         $('#main_player .displayVideo-el')[0].onclick = function() {
-            $stream_display.style.display = 'none';
+            streamers_display[0].style.display = 'none';
         };
         $('#main_player .room-info-display')[0].onclick = function() {
-            $stream_display.style.display = 'none';
+            streamers_display[0].style.display = 'none';
         };
         $('#main_player .display-mods-controls')[0].onclick = function() {
-            $stream_display.style.display = 'none';
+            streamers_display[0].style.display = 'none';
         };
-
-
-
-
-
+        var user_name, api_key, twitch_widget;
 
 
     }, 5000);
